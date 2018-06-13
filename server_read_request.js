1aeac820834267ac85d3a755eb04f7544e11ffb9
TamNguyen <huutam.ptnk@gmail.com>
2018-06-13 23:53:59 +0700

    adding fileserver.js

--- server_read_request.js
+++ server_read_request.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var url = require('url');
-
+// test with http://127.0.0.1:8080/?year=2017&month=July
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var q = url.parse(req.url, true).query;
