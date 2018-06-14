033fa02a43edc6d336668f14ebaae5388848cad4
TamNguyen <huutam.ptnk@gmail.com>
2018-06-14 13:02:43 +0700

    modify server.js

--- myserver.js
+++ myserver.js
@@ -1,11 +1,21 @@
-var http = require('http');
+const http = require('http');
 
-
-http.createServer(function (req, res){
+const server = http.createServer((req, res) => {
 	// include http header with correct content type
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	// output response to client
 	res.write('Hello World!'); 
 	//end the response
 	res.end(); 
-}).listen(8080); // server listen on port 8080
+}); 
+
+server.on('listening',(socket) => {
+	console.log('Server listening...');
+});
+
+server.on('connection',(socket) => {
+	console.log('New connection...')
+});
+
+// server listen on port 8080
+server.listen(8080);
